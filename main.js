eb2aaa3afaa45e72d573e6b118a88344813e8ead
aleksandra-migas <migas.aleksandra@gmail.com>
2022-08-28 15:44:21 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkSlide 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+        burger.classList.toggle('toggle');
+    });
+}
+navSlide();
+
+function whiteNav() {
+    let navbar = document.getElementById('navbar');
+    let scrollValue = window.scrollY;
+    if (scrollValue < 50) {
+        navbar.classList.remove('navScroll');
+    } else {
+        navbar.classList.add('navScroll');
+    }
+}
+window.addEventListener('scroll', whiteNav);
+
+
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('img')
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active')
+        const img = document.createElement('img')
+        img.src = image.src
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild)
+        }
+        lightbox.appendChild(img)
+    })
+})
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
+});
+
+
